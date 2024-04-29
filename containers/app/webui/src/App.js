66440ac95656c46439f7f8667209efab2073ac1c
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-04-29 12:25:30 +0200

    Create App.js

---
+++ containers/app/webui/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Agility Game Microservices
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
